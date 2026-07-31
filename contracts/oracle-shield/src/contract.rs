ff752c1c664d31be9a0237315e7b0443cffd567a
mbeucher <m.beucher@sunzulab.com>
2026-07-31 10:05:16 +0200

    je suis un test

--- contracts/oracle-shield/src/contract.rs
+++ contracts/oracle-shield/src/contract.rs
@@ -98,7 +98,6 @@ impl Contract {
     pub fn upgrade(env: Env, new_wasm_hash: BytesN<32>) {
         let admin: Address = env.storage().instance().get(&DataKey::Admin).unwrap();
         admin.require_auth();
-
         env.deployer().update_current_contract_wasm(new_wasm_hash);
     }
 }
